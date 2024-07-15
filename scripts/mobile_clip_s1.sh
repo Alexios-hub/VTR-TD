79edcae68f3e295487cbd6d643139d94a23b43f9
Alexios-hub <528754347@qq.com>
2024-07-15 14:52:42 +0800

    udpate

--- scripts/mobile_clip_s1.sh
+++ scripts/mobile_clip_s1.sh
@@ -6,9 +6,9 @@ torchrun --nproc_per_node 2 -m \
     --zeroshot-frequency 1 \
     --report-to tensorboard \
     --dataset-type webdataset \
-    --train-data="/home/alex/data/MSRVTT-videos/train_t_umt_preframes_12/train_{0..8}.tar"  \
+    --train-data="/home/user/data/MSRVTT-videos/train_t_umt_preframes_12/train_{0..8}.tar"  \
     --train-num-samples 9000 \
-    --val-data="/home/alex/data/MSRVTT-videos/test_t_umt_preframes_12/test_{0..1}.tar"  \
+    --val-data="/home/user/data/MSRVTT-videos/test_t_umt_preframes_12/test_{0..1}.tar"  \
     --val-num-samples 1000 \
     --warmup 0 \
     --batch-size=64 \
